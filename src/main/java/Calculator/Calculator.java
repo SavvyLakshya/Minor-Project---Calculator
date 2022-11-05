b07f359e927ce1f88a83ef08812769bf0624a078
laksh <laksh@LAPTOP-4C4RPQE3>
2022-11-05 18:26:41 +0530

    Adding files

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -108,7 +108,7 @@ Calculator(){
     
     public static void main(String[] args) {
         Calculator calc =new Calculator();
-        System.out.println("Hello");
+        
     }
 
     @Override
